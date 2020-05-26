73b6cf071c220f970051ea18b595625ea74d60a9
Matthew Wigginton Conway <matt@indicatrix.org>
2020-05-26 11:24:26 -0400

    add built fareto code.

---
+++ precache-manifest.467f501d912da204bb87821b3b6b822b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc4b6975c4d7c01a8fa810078a4035dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9be190bf8a3544b410c5",
+    "url": "/static/css/main.1e9ef4df.chunk.css"
+  },
+  {
+    "revision": "3dbd77419b082c6385ca",
+    "url": "/static/js/2.53e249f3.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/static/js/2.53e249f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9be190bf8a3544b410c5",
+    "url": "/static/js/main.63a8e626.chunk.js"
+  },
+  {
+    "revision": "23e742040fd525d6d288",
+    "url": "/static/js/runtime-main.aea0d6ba.js"
+  }
+]);
