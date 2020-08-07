6f5cc496ed104b290d7a4336f67b53c8903de703
Matthew Wigginton Conway <matt@indicatrix.org>
2020-08-07 09:59:28 -0400

    update fareto version

---
+++ precache-manifest.375cd02418528e04952f6ce5085cd22b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb59f65f1bfb64b90bfeffa25cb29245",
+    "url": "/fareto-examples/index.html"
+  },
+  {
+    "revision": "36191d5ea09dd42ac81c",
+    "url": "/fareto-examples/static/css/main.1e9ef4df.chunk.css"
+  },
+  {
+    "revision": "fec3688b5bc3c97935c0",
+    "url": "/fareto-examples/static/js/2.6868ec25.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/fareto-examples/static/js/2.6868ec25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36191d5ea09dd42ac81c",
+    "url": "/fareto-examples/static/js/main.913f2928.chunk.js"
+  },
+  {
+    "revision": "3750fa18557d1c6ced62",
+    "url": "/fareto-examples/static/js/runtime-main.ed99c799.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5b48b117c8e743a1f18dfdd1c501606e.js"
+  "/fareto-examples/precache-manifest.375cd02418528e04952f6ce5085cd22b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fareto-examples/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
